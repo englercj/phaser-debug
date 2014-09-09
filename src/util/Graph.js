c574bf7a1525f0044dfef405eecd441b1093fbc3
Chad Engler <englercj@live.com>
2014-09-09 07:37:06 -0700

    simplify graph implementation

--- src/util/Graph.js
+++ src/util/Graph.js
@@ -1,5 +1,3 @@
-//TODO: Rewrite this as a single canvas that clears each frame.
-
 function Graph(container, width, height, dataStyles) {
     this.canvas = document.createElement('canvas');
     this.canvas.width = width;
@@ -8,69 +6,61 @@ function Graph(container, width, height, dataStyles) {
 
     this.ctx = this.canvas.getContext('2d');
 
-    //setup data canvases, these are used to prerender the data graph
-    //and having two of them allows me to clear one when the other takes
-    //up the entire graph, so I have "wrap" the graph around to get more
-    this.dataCanvases = [
-        document.createElement('canvas'),
-        document.createElement('canvas')
-    ];
-    this.dataCtxs = [
-        this.dataCanvases[0].getContext('2d'),
-        this.dataCanvases[1].getContext('2d')
-    ];
-    this.dataScroll = [
-        0,
-        0
-    ];
-    this.dataIndex = 0;
-
     this.label = 'ms';
     this.labelPrecision = 0;
     this.labelStyle = 'rgba(200, 200, 200, 0.6)';
-    this.max = 50;
-    this.dataLineWidth = 1;
+
+    this.maxValue = 50;
     this.padding = 5;
 
-    this.keySize = 115;
+    this.dataLineWidth = 1;
+    this.legendWidth = 115;
 
-    this.dataCanvases[0].width = this.dataCanvases[1].width = width - this.keySize;
-    this.dataCanvases[0].height = this.dataCanvases[1].height = height;
-
-    this.data = [];
     this.styles = dataStyles || {};
 
-    if(!this.styles._default)
+    if(!this.styles._default) {
         this.styles._default = 'red';
+    }
 
-    if(!this.styles.event)
+    if(!this.styles.event) {
         this.styles.event = 'gray';
+    }
+
+    this.gbuffer = document.createElement('canvas');
+    this.gbuffer.width = width - this.dataLineWidth - this.legendWidth;
+    this.gbuffer.height = height;
+    this.gctx = this.gbuffer.getContext('2d');
 };
 
 Graph.prototype.constructor = Graph;
 
+// render the graph with the new data point
 Graph.prototype.addData = function (values) {
-    this.data.push(values);
+    // store the graph to the buffer, skipping the first line
+    this.gctx.clearRect(0, 0, this.gbuffer.width, this.gbuffer.height);
+    this.gctx.drawImage(
+        this.canvas,
+        this.legendWidth + this.dataLineWidth,
+        0,
+        this.gbuffer.width,
+        this.gbuffer.height,
+        0,
+        0,
+        this.gbuffer.height,
+        this.gbuffer.height
+    );
 
-    if(this.data.length > ((this.canvas.width - this.keySize) / this.dataLineWidth))
-        this.data.shift();
-
-    this.render();
-};
-
-Graph.prototype.render = function () {
+    // clear the main canvas
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.updateData();
-
     this.drawBg();
-    this.drawKey();
-    this.drawData();
+    this.drawLegend(values);
+    this.drawData(values);
 };
 
 Graph.prototype.drawBg = function () {
     var ctx = this.ctx,
-        minX = this.keySize,
+        minX = this.legendWidth,
         maxX = this.canvas.width,
         maxY = this.canvas.height,
         step = maxY / 3;
@@ -100,18 +90,17 @@ Graph.prototype.drawBg = function () {
     ctx.fillText((this.max / 3).toFixed(this.labelPrecision) + this.label, minX + this.padding, (step*2)-this.padding);
 };
 
-Graph.prototype.drawKey = function () {
+Graph.prototype.drawLegend = function (values) {
     var ctx = this.ctx,
         i = 0,
         box = 10,
-        data = this.data[this.data.length - 1],
         pad = this.padding,
         lbl = this.labelStyle;
 
     for(var k in this.styles) {
         var style = this.styles[k],
             y = (box * i) + (pad * (i+1)),
-            val = typeof data[k] === 'number' ? data[k].toFixed(2) : null,
+            val = typeof values[k] === 'number' ? values[k].toFixed(2) : null,
             text = k + (val ? ' (' + val + ' ms)' : '');
 
         ctx.fillStyle = style;
@@ -123,70 +112,29 @@ Graph.prototype.drawKey = function () {
     }
 };
 
-Graph.prototype.drawData = function () {
-    var i = this.dataIndex,
-        ni = this.dataIndex ? 0 : 1,
-        c1 = this.dataCanvases[i],
-        s1 = this.dataScroll[i],
-        c2 = this.dataCanvases[ni],
-        s2 = this.dataScroll[ni],
-        w = c1.width,
-        h = c1.height;
+Graph.prototype.drawData = function (values) {
+    // draw the data buffer back down, this gives the effect of "shifting" it to the left by 1
+    this.ctx.drawImage(this.gbuffer, this.legendWidth, 0);
 
-    //draw on prerender of data
-    this.ctx.drawImage(
-        c1,
-        0, //sx
-        0, //sy
-        s1, //sw
-        h, //sh
-        w - s1 + this.keySize, //dx
-        0, //dy
-        s1,
-        h
-    );
-    if(s2 - w >= 0) {
-        this.ctx.drawImage(
-            c2,
-            s2 - w, //sx
-            0, //sy
-            w - (s2 - w), //sw
-            h, //sh
-            this.keySize, //dx
-            0, //dy
-            w - (s2 - w), //dw
-            h //dh
-        );
-    }
+    // draw the new data point
+    var x = this.canvas.width - this.dataLineWidth,
+        y = this.canvas.height,
+        step = 0;
 
-    if(w === s1) {
-        this.dataScroll[ni] = this.dataLineWidth;
-        this.dataCtxs[ni].clearRect(0, 0, w, h);
-        this.dataIndex = ni;
-    }
-};
+    for(var k in values) {
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = this.ctx.fillStyle = this.styles[k] || this.styles._default;
+        this.ctx.lineWidth = this.dataLineWidth;
 
-//draw the latest data point into the dataCanvas
-Graph.prototype.updateData = function () {
-    var ctx = this.dataCtxs[this.dataIndex],
-        x = this.dataScroll[this.dataIndex],
-        maxY = this.dataCanvases[this.dataIndex].height,
-        lw = this.dataLineWidth,
-        vals = this.data[this.data.length - 1],
-        v = 0, step = 0, y = maxY;
+        if (k === 'event') {
+            if (values[k] === null) continue;
 
-    for(var k in vals) {
-        ctx.beginPath();
-        ctx.strokeStyle = ctx.fillStyle = this.styles[k] || this.styles._default;
-        ctx.lineWidth = lw;
-
-        v = vals[k];
-        if(k === 'event') {
-            ctx.moveTo(x, maxY);
-            ctx.lineTo(x, 0);
-            ctx.fillText(v, x+this.padding, (this.padding*2));
-        } else {
-            step = ((v / this.max) * maxY);
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, this.canvas.height);
+            ctx.fillText(values[k], x + this.padding, (this.padding * 2));
+        }
+        else {
+            step = ((values[k] / this.max) * this.canvas.height);
             step = step < 0 ? 0 : step;
 
             ctx.moveTo(x, y);
@@ -195,6 +143,4 @@ Graph.prototype.updateData = function () {
 
         ctx.stroke();
     }
-    this.dataScroll[0] += lw;
-    this.dataScroll[1] += lw;
 };
