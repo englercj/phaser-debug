a0d61a49ffd1129a6ecdfbe761f4505a3beae701
Chad Engler <englercj@live.com>
2014-09-11 09:08:02 -0700

    Add some color pallettes to performance

--- src/panels/Performance.js
+++ src/panels/Performance.js
@@ -10,7 +10,44 @@ function Performance(game, parent) {
     this.title = 'Performance';
     this.eventQueue = [];
 
-    this.derp = null;
+    this.colorPalettes = {
+        _default: {
+            render   : '#7cb5ec',
+            state    : '#434348',
+            stage    : '#90ed7d',
+            tweens   : '#f7a35c',
+            sound    : '#8085e9',
+            input    : '#f15c80',
+            physics  : '#e4d354',
+            particles: '#8085e8',
+            plugins  : '#8d4653',
+            event    : '#91e8e1'
+        },
+        grid: {
+            render   : '#058DC7',
+            state    : '#50B432',
+            stage    : '#ED561B',
+            tweens   : '#DDDF00',
+            sound    : '#24CBE5',
+            input    : '#64E572',
+            physics  : '#FF9655',
+            particles: '#FFF263',
+            plugins  : '#6AF9C4',
+            event    : '#7798BF'
+        },
+        unica: {
+            render   : '#2b908f',
+            state    : '#90ee7e',
+            stage    : '#f45b5b',
+            tweens   : '#7798BF',
+            sound    : '#aaeeee',
+            input    : '#ff0066',
+            physics  : '#eeaaee',
+            particles: '#55BF3B',
+            plugins  : '#DF5353',
+            event    : '#7798BF',
+        }
+    };
 }
 
 Performance.prototype = Object.create(Panel.prototype);
@@ -21,17 +58,7 @@ module.exports = Performance;
 Performance.prototype.createPanelElement = function () {
     var div = Panel.prototype.createPanelElement.call(this);
 
-    this.graph = new Graph(div, window.innerWidth - 20, 250 - 5, {
-        state    : 'rgba(220, 80, 80, 1)',
-        stage    : 'rgba(220, 80, 80, 1)',
-        tweens   : 'rgba(220, 80, 80, 1)',
-        sound    : 'rgba(80, 80, 220, 1)',
-        input    : 'rgba(80, 220, 80, 1)',
-        physics  : 'rgba(80, 220, 200, 1)',
-        particles: 'rgba(220, 220, 80, 1)',
-        plugins  : 'rgba(200, 80, 220, 1)',
-        event    : 'rgba(200, 200, 200, 0.6)'
-    });
+    this.graph = new Graph(div, window.innerWidth - 20, 250 - 5, this.colorPalettes._default);
 
     this.graph.max = 45;
 
