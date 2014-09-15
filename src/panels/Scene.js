b1f1a4515efb22eef826ca8f88c2d87c3c4c3200
Chad Engler <englercj@live.com>
2014-09-15 10:55:09 -0700

    get the details tab working

--- src/panels/Scene.js
+++ src/panels/Scene.js
@@ -5,11 +5,14 @@ var Panel = require('./Panel'),
 //require templates
 var panelHtml = require('../hbs/scene/panel.hbs'),
     detailsHtml = require('../hbs/scene/details.hbs'),
-    treeHtml = require('../hbs/scene/tree.hbs');
+    treeHtml = require('../hbs/scene/tree.hbs'),
+    _cache = {},
+    _id = 0;
 
 Handlebars.registerPartial('sceneDetails', detailsHtml);
 Handlebars.registerPartial('sceneTree', treeHtml);
 Handlebars.registerHelper('typeString', typeToString);
+Handlebars.registerHelper('listItemOpen', listItemOpen);
 
 function Scene(game, parent) {
     Panel.call(this, game, parent);
@@ -38,13 +41,19 @@ Scene.prototype.createPanelElement = function () {
 
     this.tree = this._panel.querySelector('.sidebar');
     this.details = this._panel.querySelector('.details');
-    this.refresh = this.details.querySelector('.refresh');
-
-    this.selected = this.tree.querySelector('li:first-child');
-    ui.addClass(this.selected, 'selected expanded');
+    this.refresh = this._panel.querySelector('.refresh');
 
     ui.on(this.tree, 'click', 'li', this._onLiClick.bind(this));
     ui.on(this.refresh, 'click', this._onRefreshClick.bind(this));
+
+    // this.renderer = new PIXI.CanvasRenderer(
+    //     512,
+    //     256,
+    //     document.createElement('canvas'),
+    //     true
+    // );
+
+    this.rebuildTree();
 
     return this._panel;
 };
@@ -52,30 +61,40 @@ Scene.prototype.createPanelElement = function () {
 Scene.prototype.rebuildTree = function () {
     ui.empty(this.tree);
 
+    _cache = {};
+
     this.tree.innerHTML = treeHtml(this.game.stage);
 
-    this.selected = this.tree.querySelector('li:first-child');
-    ui.addClass(this.selected, 'selected expanded');
+    this.select(this.tree.querySelector('li:first-child'));
+    ui.addClass(this.selected, 'expanded');
 
     this.reloadDetails();
 };
 
 Scene.prototype.reloadDetails = function () {
-    //TODO: cache objects with custom IDs to reload here?
-    // perhaps a hbs helper that will add to the cache and return the id?
+    var id = this.selected.dataset.id;
+
+    this.details.innerHTML = detailsHtml(_cache[id]);
+    // this.details.appendChild(this.renderer.view);
+
+    // this.renderer.renderDisplayObject(_cache[id]);
+};
+
+Scene.prototype.select = function (li) {
+    if (this.selected) {
+        ui.removeClass(this.selected, 'selected');
+    }
+
+    this.selected = li;
+    ui.addClass(this.selected, 'selected');
 };
 
 Scene.prototype._onLiClick = function (e) {
     e.stopPropagation();
 
-    if (this.selected) {
-        ui.removeClass(this.selected, 'selected');
-    }
+    this.select(e.delegateTarget);
 
-    this.selected = e.delegateTarget;
-
-    ui.addClass(this.selected, 'selected');
-    ui.toggleClass(this.selected, 'expanded');
+    ui.toggleClass(e.delegateTarget, 'expanded');
 
     this.reloadDetails();
 };
@@ -86,6 +105,14 @@ Scene.prototype._onRefreshClick = function (e) {
 
     this.rebuildTree();
 };
+
+function listItemOpen () {
+    _cache[++_id] = this;
+
+    return new Handlebars.SafeString(
+        '<li ' + (this.children && this.children.length ? 'class="has-children" ' : '') + 'data-id="' + _id + '">'
+    );
+}
 
 function typeToString () {
     var node = this;
